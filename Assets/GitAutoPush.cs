8407dfa86addc8514b59164d2256c99cfb490aeb
Won-Hyuk-Lee <lwhprt@naver.com>
2022-12-20 10:45:32 +0900

    Auto Push

--- Assets/GitAutoPush.cs
+++ Assets/GitAutoPush.cs
@@ -20,8 +20,6 @@ namespace WHBKYK
         string convertPath2;
         string line;
         string line2;
-        string gitHubAddress;
-        //string gitLocalAddress;
         string gitBranch;
         string gitCommit;
         string gitCommited;
@@ -56,11 +54,8 @@ namespace WHBKYK
             convertPath = UnityPath.Replace("/", "\\");
             convertPath2 = convertPath.Replace("\\Assets", "");
 
-            EditorGUILayout.LabelField("사용 설명서 : 깃허브 연결되어있는 상태로 로컬 저장소 주소를 입력한 뒤 커밋 메시지를 입력하고 푸쉬해보세요.");
+            EditorGUILayout.LabelField("사용 설명서 : 깃허브 연결되어있는 상태라면 커밋 메시지를 입력하고 푸쉬해보세요.");
             EditorGUILayout.LabelField("");
-            EditorGUILayout.LabelField("");
-
-            //gitLocalAddress = EditorGUILayout.TextField("로컬저장소링크", gitLocalAddress, GUILayout.Width(500f));
 
             if (GUILayout.Button("현재 연결된 브랜치 확인", GUILayout.Width(150f)))
             {
@@ -76,21 +71,22 @@ namespace WHBKYK
             }
             EditorGUILayout.LabelField(line2);
             EditorGUILayout.LabelField("");
-            /*
+            
             gitBranch = EditorGUILayout.TextField("변경할 브랜치 명", gitBranch, GUILayout.Width(500f));
             
             if (GUILayout.Button("Switch", GUILayout.Width(150f)))
             {
                 if (gitBranch == null) return;
                 StreamWriter sw = new StreamWriter(path + switchBranch);
-                sw.WriteLine("cd " + gitLocalAddress);
+                sw.WriteLine("cd " + convertPath2);
+                sw.WriteLine("git " + gitBranch);
                 sw.WriteLine("git switch " + gitBranch);
                 sw.Flush();
                 sw.Close();
                 Process.Start(path + switchBranch);
             }
             EditorGUILayout.LabelField("");
-            */
+            
             gitCommit = EditorGUILayout.TextField("커밋메시지 입력", gitCommit, GUILayout.Width(500f));
             if (GUILayout.Button("Commit", GUILayout.Width(150f)))
             {
@@ -115,7 +111,7 @@ namespace WHBKYK
                 Process.Start(path + pushBat).WaitForExit();
             }
             EditorGUILayout.LabelField("");
-            //gitHubAddress = EditorGUILayout.TextField("깃허브링크", gitHubAddress, GUILayout.Width(500f));
+            
         }
 
     }
